8ab2c7e18291a0ecf821a868c0b6d7500ee78189
Alex <adahl1993@gmail.com>
2024-05-06 18:21:42 -0500

    finished base goals

--- script.js
+++ script.js
@@ -17,4 +17,112 @@ function onReady() {
 }
 
 
-onReady()
+onReady();
+let playerAP = 100;
+let fungusHP = 100;
+let arcaneScepter = {
+    ap: 12,
+    hpdmg: 14,
+};
+let entangle = {
+    ap: 23,
+    hpdmg: 9,
+};
+let dragonBlade = {
+    ap:38,
+    hpdmg: 47,
+};
+let starFire = {
+    ap: 33,
+    hpdmg: 25,
+};
+
+function arcaneATK(event) {
+    event.target;
+playerAP -= arcaneScepter.ap;
+fungusHP -= arcaneScepter.hpdmg;
+document.getElementById('action-points').innerHTML = `${playerAP} AP`;
+document.getElementById('health-points').innerHTML = `${fungusHP} HP`;
+    if(playerAP < 0) {
+        playerAP = 0;
+        document.getElementById('action-points').innerHTML = `${playerAP} AP`;
+        document.getElementById('fungus-move').className = "freaky-fungus jump";
+        document.getElementById('arcane-btn').disabled = true;
+        document.getElementById('entangle-btn').disabled = true;
+        document.getElementById('dragon-btn').disabled = true;
+        document.getElementById('star-btn').disabled = true;
+    }
+    if(fungusHP < 0) {
+        fungusHP = 0;
+        document.getElementById('health-points').innerHTML = `${fungusHP} HP`;
+        document.getElementById('fungus-move').className = "freaky-fungus dead";
+    }
+}
+
+function entangleATK(event) {
+    event.target;
+    playerAP -= entangle.ap;
+    fungusHP -= entangle.hpdmg;
+    document.getElementById('action-points').innerHTML = `${playerAP} AP`;
+document.getElementById('health-points').innerHTML = `${fungusHP} HP`;
+if(playerAP < 0) {
+    playerAP = 0;
+    document.getElementById('action-points').innerHTML = `${playerAP} AP`;
+    document.getElementById('fungus-move').className = "freaky-fungus jump";
+    document.getElementById('arcane-btn').disabled = true;
+    document.getElementById('entangle-btn').disabled = true;
+    document.getElementById('dragon-btn').disabled = true;
+    document.getElementById('star-btn').disabled = true;
+
+
+}
+if(fungusHP < 0) {
+    fungusHP = 0;
+    document.getElementById('health-points').innerHTML = `${fungusHP} HP`;
+    document.getElementById('fungus-move').className = "freaky-fungus dead";
+}
+}
+
+function dragonBladeATK(event) {
+    event.target;
+playerAP -= dragonBlade.ap;
+fungusHP -= dragonBlade.hpdmg;
+document.getElementById('action-points').innerHTML = `${playerAP} AP`;
+document.getElementById('health-points').innerHTML = `${fungusHP} HP`;
+if(playerAP < 0) {
+    playerAP = 0;
+    document.getElementById('action-points').innerHTML = `${playerAP} AP`;
+    document.getElementById('fungus-move').className = "freaky-fungus jump";
+    document.getElementById('arcane-btn').disabled = true;
+    document.getElementById('entangle-btn').disabled = true;
+    document.getElementById('dragon-btn').disabled = true;
+    document.getElementById('star-btn').disabled = true;
+}
+if(fungusHP < 0) {
+    fungusHP = 0;
+    document.getElementById('health-points').innerHTML = `${fungusHP} HP`;
+    document.getElementById('fungus-move').className = "freaky-fungus dead";
+}
+}
+
+function starFireATK(event) {
+    event.target;
+playerAP -= starFire.ap;
+fungusHP -= starFire.hpdmg;
+document.getElementById('action-points').innerHTML = `${playerAP} AP`;
+document.getElementById('health-points').innerHTML = `${fungusHP} HP`;
+if(playerAP < 0) {
+    playerAP = 0;
+    document.getElementById('action-points').innerHTML = `${playerAP} AP`;
+    document.getElementById('fungus-move').className = "freaky-fungus jump";
+    document.getElementById('arcane-btn').disabled = true;
+    document.getElementById('entangle-btn').disabled = true;
+    document.getElementById('dragon-btn').disabled = true;
+    document.getElementById('star-btn').disabled = true;
+}
+if(fungusHP < 0) {
+    fungusHP = 0;
+    document.getElementById('health-points').innerHTML = `${fungusHP} HP`;
+    document.getElementById('fungus-move').className = "freaky-fungus dead";
+}
+}
